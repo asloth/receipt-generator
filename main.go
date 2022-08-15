ba8dc82144885995bec19e797bdf823ef7136817
asloth <sbenelramirez@gmail.com>
2022-08-15 02:13:11 -0500

    feat: extracting data from excel into struct

--- main.go
+++ main.go
@@ -2,39 +2,90 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/xuri/excelize/v2"
 )
 
 func main() {
-	f, err := excelize.OpenFile("Book1.xlsx")
+	// fechaEmision := "22/07/2022"
+	// fechaVenc := "31/07/2022"
+	// tipoCuota := "ORDINARIO"
+	finalColumn := 11
+	// startColumn := "A"
+	totalNumberOfRows := 210
+
+	filePath := "files/GPR-CUOTA-JULIO-2022.xlsx"
+
+	sheetName := "Propietarios ordenados"
+
+	xlsxFile, err := excelize.OpenFile(filePath)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer func() {
 		// Close the spreadsheet.
-		if err := f.Close(); err != nil {
+		if err := xlsxFile.Close(); err != nil {
 			fmt.Println(err)
 		}
 	}()
-	// Get value from cell by given worksheet name and axis.
-	cell, err := f.GetCellValue("Sheet1", "B2")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(cell)
 	// Get all the rows in the Sheet1.
-	rows, err := f.GetRows("Sheet1")
+	rows, err := xlsxFile.GetRows(sheetName)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	for _, row := range rows {
-		for _, colCell := range row {
-			fmt.Print(colCell, "\t")
+	cols := []string{}
+	ret := []Apartment{}
+	for i, row := range rows {
+		if i == 0 {
+			for _, colCell := range row {
+				// cols = strings.Split(colCell, " ")
+				cols = append(cols, colCell)
+			}
+			fmt.Println("Column information", cols)
+		} else {
+			var ap Apartment
+			for j, colCell := range row {
+				if j > finalColumn {
+					break
+				}
+
+				switch cols[j] {
+				case "propietario":
+					ap.owner = colCell
+				case "depa":
+					ap.number, err = strconv.ParseInt(colCell, 10, 64)
+				case "total área":
+					ap.totalArea, err = strconv.ParseFloat(colCell, 10)
+				case "cuota":
+					ap.amount, err = strconv.ParseFloat(colCell, 10)
+				case "porcentaje":
+					ap.percentaje, err = strconv.ParseFloat(colCell, 10)
+				case "estaciona":
+					if len(colCell) == 0 {
+						colCell = "--"
+					}
+					ap.parking = colCell
+				}
+			}
+			ret = append(ret, ap)
 		}
-		fmt.Println()
+		if i > totalNumberOfRows {
+			break
+		}
+
 	}
+	fmt.Println(ret[2])
+
+}
+
+type Apartment struct {
+	number     int64
+	owner      string
+	totalArea  float64
+	percentaje float64
+	amount     float64
+	parking    string
 }
