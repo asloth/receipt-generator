dfe184c2be7b06a1ae4f357d527543c4ce8a7a44
asloth <sbenelramirez@gmail.com>
2022-08-20 22:35:41 -0500

    feat(receipt): add payment data and notes to receipt

--- apartment.go
+++ apartment.go
@@ -14,49 +14,14 @@ func main() {
 	var contentSize float64 = 10
 	var rowHeight float64 = 7
 	colorMolio := color.Color{
-		Red:   96,
-		Green: 237,
-		Blue:  26,
+		Red:   148,
+		Green: 235,
+		Blue:  66,
 	}
 	backgroundColor := color.NewWhite()
 	m := pdf.NewMaroto(consts.Portrait, consts.A4)
 	// Header
-	m.Row(heightHeader, func() {
-		m.Col(4, func() {
-			_ = m.FileImage("files/molio-logo.jpg", props.Rect{
-
-				Center: true,
-			})
-		})
-		m.Col(4, func() {
-			m.Text("RECIBO DE MANTENIMIENTO", props.Text{
-				Size:  12,
-				Style: consts.Bold,
-				Align: consts.Center,
-				Top:   9,
-				Color: color.Color{
-					Red: 255,
-				},
-			})
-			m.Text("CONDOMINIO GRAN PARQUE ROMA", props.Text{
-				Size:  10,
-				Style: consts.Bold,
-				Align: consts.Center,
-				Top:   15,
-			})
-			m.Text("LEONARDO ARIETA 825 - CERCADO DE LIMA", props.Text{
-				Size:  8,
-				Style: consts.Bold,
-				Align: consts.Center,
-				Top:   20,
-			})
-		})
-		m.Col(4, func() {
-			_ = m.FileImage("files/parque-roma-logo.jpg", props.Rect{
-				Center: true,
-			})
-		})
-	})
+	receiptHeader(&m, heightHeader)
 
 	// tabla inicial
 	headers := []string{"TIPO CUOTA", "F. EMISION", "F. VCTO.", "PERIODO", "N. RECIBO"}
@@ -91,133 +56,12 @@ func main() {
 	})
 
 	// tabla central datos del usuario
-	m.SetBackgroundColor(colorMolio)
-	m.Row(7, func() {
-		m.Col(12, func() {
-			m.Text("DATOS DEL PROPIETARIO/INQUILINO",
-				props.Text{
-					Size:  12,
-					Style: consts.Bold,
-					Align: consts.Center,
-				})
-		})
-	})
-	m.SetBackgroundColor(backgroundColor)
-	m.SetBorder(false)
-	var column1 uint = 4
-	var columnData uint = 8
-	m.Row(rowHeight, func() {
-		m.Col(column1, func() {
-			m.Text("NOMBRE: ", props.Text{
-				Size:            contentSize,
-				Align:           consts.Right,
-				Style:           consts.Bold,
-				VerticalPadding: 3,
-				Top:             1,
-			})
-		})
-		m.Col(columnData, func() {
-			m.Text(owner, props.Text{
-				Top: 1,
-			})
-		})
-	})
-	m.Row(rowHeight, func() {
-		m.Col(column1, func() {
-			m.Text("DEPARTAMENTO: ", props.Text{
-				Size:            contentSize,
-				Align:           consts.Right,
-				Style:           consts.Bold,
-				VerticalPadding: 3,
-				Top:             1,
-			})
-		})
-		m.Col(columnData, func() {
-			m.Text(owner, props.Text{
-				Top: 1,
-			})
-		})
-	})
-	m.Row(rowHeight, func() {
-		m.Col(column1, func() {
-			m.Text("CODIGO BANCO: ", props.Text{
-				Size:            contentSize,
-				Align:           consts.Right,
-				Style:           consts.Bold,
-				VerticalPadding: 3,
-				Top:             1,
-			})
-		})
-		m.Col(columnData, func() {
-			m.Text(owner, props.Text{
-				Top: 1,
-			})
-		})
-	})
-	m.Row(rowHeight, func() {
-		m.Col(column1, func() {
-			m.Text("ESTACIONAMIENTO: ", props.Text{
-				Size:            contentSize,
-				Align:           consts.Right,
-				Style:           consts.Bold,
-				VerticalPadding: 3,
-				Top:             1,
-			})
-		})
-		m.Col(columnData, func() {
-			m.Text(owner, props.Text{
-				Top: 1,
-			})
-		})
-	})
-	m.Row(rowHeight, func() {
-		m.Col(column1, func() {
-			m.Text("AREA DEPARTAMENTO: ", props.Text{
-				Size:            contentSize,
-				Align:           consts.Right,
-				Style:           consts.Bold,
-				VerticalPadding: 3,
-				Top:             1,
-			})
-		})
-		m.Col(columnData, func() {
-			m.Text(owner, props.Text{
-				Top: 1,
-			})
-		})
-	})
-	m.Row(rowHeight, func() {
-		m.Col(column1, func() {
-			m.Text("AREA TOTAL DEL EDIFICIO: ", props.Text{
-				Size:            contentSize,
-				Align:           consts.Right,
-				Style:           consts.Bold,
-				VerticalPadding: 3,
-				Top:             1,
-			})
-		})
-		m.Col(columnData, func() {
-			m.Text(owner, props.Text{
-				Top: 1,
-			})
-		})
-	})
-	m.Row(rowHeight, func() {
-		m.Col(column1, func() {
-			m.Text("TOTAL PRESUPUESTO: ", props.Text{
-				Size:            contentSize,
-				Align:           consts.Right,
-				Style:           consts.Bold,
-				VerticalPadding: 3,
-				Top:             1,
-			})
-		})
-		m.Col(columnData, func() {
-			m.Text(owner, props.Text{
-				Top: 1,
-			})
-		})
-	})
+	subHeader(&m, colorMolio, "DATOS DEL PROPIETARIO/INQUILINO")
+
+	atributes := []string{"NOMBRE: ", "DEPARTAMENTO: ", "CODIGO BANCO: ", "AREA DEPARTAMENTO: ", "ESTACIONAMIENTO: ", "% PARTICIPACION: ", "AREA TOTAL EDIFICIO: ", "TOTAL PRESUPUESTO: "}
+	for _, v := range atributes {
+		dataOwner(&m, backgroundColor, rowHeight, contentSize, v, owner)
+	}
 
 	//TERCERA TABLA de importes fcturados
 	m.SetBackgroundColor(colorMolio)
@@ -261,5 +105,162 @@ func main() {
 		})
 	})
 
+	m.SetBackgroundColor(colorMolio)
+	m.SetBorder(true)
+	m.Row(7, func() {
+		m.Col(10, func() {
+			m.Text("TOTAL A PAGAR S/.",
+				props.Text{
+					Size:  12,
+					Style: consts.Bold,
+					Align: consts.Center,
+				})
+		})
+		m.Col(2, func() {
+			m.Text("169.24",
+				props.Text{
+					Size:  12,
+					Style: consts.Bold,
+					Align: consts.Center,
+				})
+		})
+	})
+	m.SetBackgroundColor(backgroundColor)
+	m.Row(7, func() {})
+
+	subHeader(&m, colorMolio, "INFORMACION DE PAGO")
+	payInfo(&m, colorMolio)
+
+	//FOOTER : AVISOS IMPORTANTES DE LA BOLETA
+	subHeader(&m, colorMolio, "AVISO IMPORTANTE")
+	m.SetBackgroundColor(backgroundColor)
+	m.Row(10, func() {
+		m.Col(12, func() {
+			m.Text("1. Se deja por escrito que el incumplimiento del pago esta sujeto a mora.",
+				props.Text{
+					Size:  contentSize,
+					Align: consts.Left,
+				})
+			m.Text("2. El Propietario autoriza el corte de suministrode agua por incumplimiento de pago.",
+				props.Text{
+					Size:  contentSize,
+					Align: consts.Left,
+					Top:   5,
+				})
+		})
+	})
+
 	m.OutputFileAndClose("maroto.pdf")
+}
+
+func receiptHeader(pdf *pdf.Maroto, heightHeader float64) {
+	m := *pdf
+	var colWidth uint = 4
+
+	m.Row(heightHeader, func() {
+		m.Col(colWidth, func() {
+			_ = m.FileImage("files/molio-logo.jpg", props.Rect{
+				Center: true,
+			})
+		})
+		m.Col(colWidth, func() {
+			m.Text("RECIBO DE MANTENIMIENTO", props.Text{
+				Size:  12,
+				Style: consts.Bold,
+				Align: consts.Center,
+				Top:   9,
+				Color: color.Color{
+					Red: 255,
+				},
+			})
+			m.Text("CONDOMINIO GRAN PARQUE ROMA", props.Text{
+				Size:  10,
+				Style: consts.Bold,
+				Align: consts.Center,
+				Top:   15,
+			})
+			m.Text("LEONARDO ARIETA 825 - CERCADO DE LIMA", props.Text{
+				Size:  8,
+				Style: consts.Bold,
+				Align: consts.Center,
+				Top:   20,
+			})
+		})
+		m.Col(colWidth, func() {
+			_ = m.FileImage("files/parque-roma-logo.jpg", props.Rect{
+				Center: true,
+			})
+		})
+	})
+}
+
+func dataOwner(pdf *pdf.Maroto, backgroundColor color.Color, rowHeight float64, contentSize float64, prop, data string) {
+	m := *pdf
+	m.SetBackgroundColor(backgroundColor)
+	m.SetBorder(false)
+	var column1 uint = 4
+	var columnData uint = 8
+	m.Row(rowHeight, func() {
+		m.Col(column1, func() {
+			m.Text(prop, props.Text{
+				Size:            contentSize,
+				Align:           consts.Right,
+				Style:           consts.Bold,
+				VerticalPadding: 3,
+				Top:             1,
+			})
+		})
+		m.Col(columnData, func() {
+			m.Text(data, props.Text{
+				Top: 1,
+			})
+		})
+	})
+}
+
+func payInfo(pdf *pdf.Maroto, colorMolio color.Color) {
+	m := *pdf
+
+	headers := []string{"BANCO", "CUENTA BANCARIA", "TITULAR DE CUENTA", "NUMERO INTERBANCARIO"}
+	contents := [][]string{
+		{"BCP", "3059864512041", "C. RECAUDADORA GRAN PARQUE ROMA", "********************"},
+	}
+
+	m.TableList(headers, contents, props.TableList{
+		HeaderProp: props.TableListContent{
+			Family:    consts.Arial,
+			Style:     consts.Bold,
+			Size:      11.0,
+			GridSizes: []uint{2, 3, 4, 3},
+		},
+		ContentProp: props.TableListContent{
+			Family:    consts.Courier,
+			Style:     consts.Normal,
+			Size:      10.0,
+			GridSizes: []uint{2, 3, 4, 3},
+		},
+		Align:                  consts.Center,
+		HeaderContentSpace:     0.01,
+		VerticalContentPadding: 4.0,
+		AlternatedBackground: &color.Color{
+			Red:   255,
+			Green: 255,
+			Blue:  255,
+		},
+	})
+}
+
+func subHeader(pdf *pdf.Maroto, colorMolio color.Color, subtitulo string) {
+	m := *pdf
+	m.SetBackgroundColor(colorMolio)
+	m.Row(7, func() {
+		m.Col(12, func() {
+			m.Text(subtitulo,
+				props.Text{
+					Size:  12,
+					Style: consts.Bold,
+					Align: consts.Center,
+				})
+		})
+	})
 }
